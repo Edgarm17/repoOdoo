dc8adeae86895eed99b6b83230855bd8b8094122
Edgar <edgaralkugm@gmail.com>
2019-11-21 16:37:20 +0100

    Models i vistes #1
    Creació del jugador

--- modules/game/models/models.py
+++ modules/game/models/models.py
@@ -2,11 +2,24 @@
 
 from odoo import models, fields, api
 
-
 class game(models.Model):
-
      _name = 'game.game'
      name = fields.Char()
-     value = fields.Integer()
+
+
+class jugador(models.Model):
+
+     _name = 'game.jugador'
+     nombre = fields.Char()
+     raza = fields.Selection([('1','Hombres'),('2','Elfos'),('3','Enanos'),('4','Orcos')])
+     bando = fields.Selection([('1', 'Luz'), ('2', 'Oscuridad')])
+     imagen = fields.Binary()
+
+
+
+
+
+
+
 
 
