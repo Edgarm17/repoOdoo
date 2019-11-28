a82597dce5eff4faa2b703df106a4074b8374a00
Edgar <edgaralkugm@gmail.com>
2019-11-28 17:41:54 +0100

    Models i vistes #2
    Creació dels models material i materials

--- modules/joc/models/models.py
+++ modules/joc/models/models.py
@@ -14,3 +14,22 @@ class jugador(models.Model):
     raza = fields.Selection([('1', 'Hombres'), ('2', 'Elfos'), ('3', 'Enanos'), ('4', 'Orcos')])
     bando = fields.Selection([('1', 'Luz'), ('2', 'Oscuridad')])
     imagen = fields.Binary()
+    raws = fields.One2many('joc.materiales','jugador')
+
+
+
+class materiales(models.Model):
+    _name = 'joc.materiales'
+    nombre = fields.Char()
+    cantidad = fields.Float()
+    jugador = fields.Many2one('joc.jugador')
+    material = fields.Many2one('joc.material')
+
+
+class material(models.Model):
+    _name = 'joc.material'
+    nombre = fields.Char()
+    imagen = fields.Binary()
+
+
+
