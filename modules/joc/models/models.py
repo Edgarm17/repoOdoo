8a8617e4e3c74ff80ffc39ee872ac9441674a0ce
Edgar <edgaralkugm@gmail.com>
2019-12-05 16:41:30 +0100

    Models i vistes #4

--- modules/joc/models/models.py
+++ modules/joc/models/models.py
@@ -10,43 +10,22 @@ class game(models.Model):
 
 class jugador(models.Model): #Clientes de odoo
     _name = 'joc.jugador'
-    nombre = fields.Char()
+    name = fields.Char()
+    image = fields.Binary()
     raza = fields.Selection([('1', 'Hombres'), ('2', 'Elfos'), ('3', 'Enanos'), ('4', 'Orcos')])
     bando = fields.Selection([('1', 'Luz'), ('2', 'Oscuridad')])
-    imagen = fields.Binary()
-    materiales = fields.One2many('joc.materiales','jugador')
+    materiales = fields.One2many('joc.materiales', 'jugador')
 
-class produccion(models.Model): #Tabla de produccion de recursos
-    _name = 'joc.producion'
-    nombre = fields.Char()
-    recurso = fields.Many2one('joc.recurso') #El recurso que produce
-    nivel = fields.Integer() #Cuánto consume en cada producción del recurso segun el nivel
-    material = fields.Many2one('joc.material') # El material que gasta para producir
-    produccion = fields.Float() #Número de producciones por unidad de tiempo
-
-class coste(models.Model):
-    _name = 'joc.coste'
-    nombre = fields.Char()
-    recurso = fields.Many2one('joc.recurso')
-    nivel = fields.Integer() #Indica lo que consume en cada nivel
-    material = fields.Many2one('joc.material') #Material del coste
-    coste = fields.Float() #Coste
-
-class recurso(models.Model):
-    _name = 'joc.recurso'
-    nombre = fields.Char()
-    nivel = fields.Float()
-    imagen = fields.Binary()
-    producciones = fields.One2many('joc.produccion','recurso')
-    coste = fields.One2many('joc.coste','recurso')
-
-class material(models.Model):
-    _name = 'joc.material'
-    nombre = fields.Char()
-    imagen = fields.Binary()
+class materiales(models.Model):#Materiales de un jugador
+    _name = 'joc.materiales'
+    material = fields.Many2one('joc.material')
     cantidad = fields.Float()
     jugador = fields.Many2one('joc.jugador')
 
+class material(models.Model):#Material
+    _name = 'joc.material'
+    name = fields.Char()
+    image = fields.Binary()
 
 
 
