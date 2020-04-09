faf3906f615861034b32a161f23ec81eda179ca8
Edgar <edgaralkugm@gmail.com>
2020-04-09 08:07:16 -0700

    Commit 1

--- modules/joc/models/models.py
+++ modules/joc/models/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-
+from datetime import datetime, timedelta
 
 class game(models.Model):
     _name = 'joc.joc'
@@ -12,25 +12,42 @@ class jugador(models.Model): #Clientes de odoo
     _name = 'joc.jugador'
     name = fields.Char()
     image = fields.Binary()
-    atac = fields.Float()
+
+    data_creacio = fields.Datetime(default=lambda self: fields.Datetime.now())
+    atac = fields.Float(string="Atac jugador", compute="get_atac")
     defensa = fields.Float()
     gold = fields.Float()
     ferro = fields.Float()
     madera = fields.Float()
     pedra = fields.Float()
     nivell = fields.Float()
-
+    experiencia = fields.Text(string="Experiencia jugador", compute="get_experiencia")
     #CLAUS ALIENES
 
     atacants = fields.One2many('joc.atacants','jugador')
+    atacantk = fields.One2many(related='atacants')
     defenses = fields.One2many('joc.defenses','jugador')
     mines = fields.One2many('joc.mines','jugador')
     event = fields.Many2many('joc.event','jugador')
 
+    #FUNCIONS
+
+    @api.depends('atac')
+    def get_atac(self):
+        for i in self:
+            for r in i.atacants:
+                i.atac += (r.atacant.atac * r.cantitat)
+
+    @api.depends('experiencia')
+    def get_experiencia(self):
+        for i in self:
+            data_actual = datetime.now()
+            diferencia = data_actual - i.data_creacio
+            i.experiencia = "Hores d'experiència: "+str(int(diferencia.seconds / 3600))+" h."
+
 class atacants(models.Model):
     _name = 'joc.atacants'
     cantitat = fields.Float()
-
     #CLAUS ALIENES
 
     atacant = fields.Many2one('joc.atacant')
@@ -43,7 +60,7 @@ class atacant(models.Model):
     atac = fields.Float()
     vida = fields.Float()
     cost = fields.Float()
-    nivell = fields.Float()
+
 
 class defenses(models.Model):
     _name = 'joc.defenses'
